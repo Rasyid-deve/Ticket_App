2bbf43a43863f96605cf4f749eac4fb1845521a5
Rasyid-deve <renaldirasyed@gmail.com>
2023-12-12 17:16:48 +0700

    updated ticket form

--- ticket-apps/app/layout.js
+++ ticket-apps/app/layout.js
@@ -1,6 +1,6 @@
 import Nav from './(components)/Nav';
-import { Inter } from 'next/font/google';
 import './globals.css';
+import { Inter } from 'next/font/google';
 
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div classname="flex flex-col h-screen max-h-screen">
+        <div className="flex flex-col h-screen max-h-screen">
           <Nav />
           <div className="flex-grow overflow-y-auto bg-page text-default-text">{children}</div>
         </div>
