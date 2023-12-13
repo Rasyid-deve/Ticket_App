69be42fc950abd690fb49f845cab256f4c82d7f3
Rasyid-deve <renaldirasyed@gmail.com>
2023-12-13 07:40:46 +0700

    updated still progress

--- ticket-apps/app/(components)/DeleteBlock.jsx
+++ ticket-apps/app/(components)/DeleteBlock.jsx
@@ -2,6 +2,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
 
 const DeleteBlock = () => {
+  const deleteTicket = async () => {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`);
+  };
+
   return <FontAwesomeIcon icon={faX} className="text-red 400 hover:cursor-pointer hover:text-red-200" />;
 };
 
